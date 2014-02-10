bab9752b0b03c5875f33e396ce4decdeb841afe3
Bryan Clement <bclement01@gmail.com>
2014-02-10 11:27:55 -0800

    added a stock secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackerinos::Application.config.secret_key_base = '90f39bc17b3b025e73b83e0ac5a8d163da3180b8be633fb570f0be4c1d1a994330e023112bc2c2a032e66bf615e85c9c93e6abb50f972192df3312ff75718a13'
